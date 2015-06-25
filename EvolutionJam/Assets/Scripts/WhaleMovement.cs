9ba0c18ea00874538ef5fdee16f5d43109cefdb9
atesija <antesija@gmail.com>
2015-06-25 19:17:22 -0400

    Havin a whale of a time

---
+++ EvolutionJam/Assets/Scripts/WhaleMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhaleMovement : MonoBehaviour {
+
+	Vector3 startPosition;
+	public float _velocity = 1.0f;
+	public Transform finalPosition;
+	public Transform stageTop;
+	public Transform stageBottom;
+	public float minScale = 1.2f;
+	public float maxScale = 2.5f;
+
+	// Use this for initialization
+	void Start () {
+		startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 position = transform.position;
+		position.x -= _velocity * Time.deltaTime;
+		transform.position = position;
+
+		if (transform.position.x < finalPosition.position.x)
+		{
+			transform.position = startPosition;
+			float whaleHeight = Random.Range(stageBottom.position.y, stageTop.position.y);
+			Vector3 whalePosition = new Vector3(transform.position.x, whaleHeight, transform.position.z);
+			transform.position = whalePosition;
+
+			float scale = Random.Range(minScale, maxScale);
+			Vector3 whaleScale = new Vector3(scale, scale, scale);
+			transform.localScale = whaleScale;
+		}
+	}
+}
