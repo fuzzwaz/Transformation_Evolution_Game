e0c150662597ab4ea7dbdfd43e14a78bb328d3c2
atesija <antesija@gmail.com>
2015-06-28 16:27:21 -0400

    Loading between scenes

---
+++ EvolutionJam/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour {
+
+	public string sceneToLoad;
+
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Start"))
+		{
+			Debug.Log("Loading " + sceneToLoad);
+			Application.LoadLevel(sceneToLoad);
+		}
+	}
+}
