13094d8669d8a1d24bd1bacf58fec0986b8f6dbd
grego118 <grego118@users.noreply.github.com>
2015-06-29 12:43:49 -0400

    Added Player.cs

---
+++ EvolutionJam/Assets/Scripts/AI/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FuzzyEvolutions
+{
+   public class Player
+   {
+      private readonly ICollection<string> _evolutionLabels;
+
+      public Player(int playerNumber)
+      {
+         PlayerNumber = playerNumber;
+         _evolutionLabels = new List<string>();
+      }
+
+      public int PlayerNumber
+      {
+         get;
+
+         private set;
+      }
+
+      public void AddEvolution(string label)
+      {
+         _evolutionLabels.Add(label);
+      }
+
+      public bool HasEvolution(string label)
+      {
+         return _evolutionLabels.Contains(label);
+      }
+   }
+}
