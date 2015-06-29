6ab1e862f173a4242687dd122f2440d2b0c5d8a3
atesija <antesija@gmail.com>
2015-06-28 21:05:50 -0400

    Scene change logic

---
+++ EvolutionJam/Assets/Scripts/PlayerWinDecipherer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerWinDecipherer : MonoBehaviour {
+
+	public Text playerNumber;
+
+	// Use this for initialization
+	void Start () {
+		playerNumber.text = "PLAYER " + "99";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
